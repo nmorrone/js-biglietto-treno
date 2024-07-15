a61897698f72d8e6541ede611b3d4a60460fc256
nmorrone <n.morrone96@gmail.com>
2024-07-15 20:54:22 +0200

    distance no discount

---
+++ JS/app.js
@@ -0,0 +1,16 @@
+console.log('Calcola del Prezzo del Biglietto')
+
+//Faccio inserire all'utente il chilometraggio del viaggio
+
+let distance = prompt('Quanti Km devi percorrere?')
+
+console.log(distance)
+
+//calcola il prezzo del biglietto senza eventuali sconti
+
+ticketDraft = distance * 0.21
+
+console.log(ticketDraft)
+
+/* comincio a calcolare eventuali sconti sul prezzo
+Chiedo all'utente la sua età */
